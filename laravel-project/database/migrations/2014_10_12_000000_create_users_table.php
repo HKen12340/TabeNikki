cb2d7293fa0d77409cbaa966a1f8fbaf97ed0998
HKen12340 <ke.hontani@gmail.com>
2025-11-02 22:43:53 +0900

    マイグレーション 作成

--- laravel-project/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,30 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('food_name');
+            $table->string('shop_name');
+            $table->string('price');
+            $table->text('place');
+            $table->text('thoughts');
+            $table->timestamps();
+            $table->foreignId('user_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
+
+        Schema::create('images',function(Blueprint $table){
+            $table->id();
+            $table->text('food_img');
+            $table->text('shop_img');
+            $table->foreignId('content_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+        });
     }
 
     /**
@@ -27,6 +51,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('images');
+        Schema::dropIfExists('contents');
         Schema::dropIfExists('users');
     }
 };
