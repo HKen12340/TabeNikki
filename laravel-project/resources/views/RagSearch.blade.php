7653cf3de62cdb6614506cbeb6aecc76dfde38fa
HKen12340 <ke.hontani@gmail.com>
2026-04-02 23:07:34 +0900

    RAG検索画面 デザイン追加

--- laravel-project/resources/views/RagSearch.blade.php
+++ laravel-project/resources/views/RagSearch.blade.php
@@ -3,15 +3,24 @@
 @section('title','RAG検索')
 
 @section('content')
-<h2>RAG検索</h2>
-<form action="{{route('RAGSearch')}}" method="POST">
-    @csrf
-    <textarea cols="60" rows="8" name="q"></textarea>
-    <input type="submit" />
-</form>
+<div class="m-5">
+    <div class="border p-4 col-7 m-auto">
+        <div class="col-2 col-7 m-auto">
+            <h2>RAG検索</h2>
+            <form action="{{route('RAGSearch')}}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <textarea cols="30" rows="8" name="q" classmaxlength="100" class="form-control mb-3"></textarea>
+                    <input type="submit"  class="btn btn-primary">
+                </div>
+            </form>
 
-
-<div>
-    {{$answer ?? ''}}
+            @if(!empty($answer))
+                <div class="border p-4 m-auto">
+                    {{$answer}}
+                </div>
+            @endif
+        </div>
+    </div>
 </div>
 @endsection
