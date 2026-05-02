f88f381fef0756eb42d00b4d9df051c46a561be9
HKen12340 <ke.hontani@gmail.com>
2026-05-02 22:57:15 +0900

    ページレイアウト 修正

--- laravel-project/resources/views/EditData.blade.php
+++ laravel-project/resources/views/EditData.blade.php
@@ -19,37 +19,60 @@
                 @method('PATCH')
                 @csrf
                 <div class="form-grou mb-3">
+                    <label>料理名:</label>
                     <input type="text" name="food_name" maxlength="100" class="form-control" value={{$item->food_name}} required>
                 </div>
                 <div class="form-grou mb-3">
-                    <img src={{ asset(optional($item->image)->food_img) }} alt="">
-                </div>
-                <div class="form-grou mb-3">
+                    <label>店名:</label>
                     <input type="text" name="shop_name" class="form-control" maxlength="100" value={{$item->shop_name}} required>
                 </div>
                 <div class="form-grou mb-3">
+                    <label for="">料金:</label>
                     <input type="number" name="price" class="form-control" value={{$item->price}} required>
                 </div>
                         
                 <div class="form-grou mb-3">
+                    <label for="">来店日:</label>
                     <input type="date" name="visit_date" class="form-control" value={{$item->visit_date}} required>
                 </div>
                 <div class="form-grou mb-3">
+                    <label for="">場所:</label>
                     <input type="text" name="place" maxlength="100" class="form-control" value={{$item->place}} required>
                 </div>
                 <div class="form-grou mb-3">
+                    <label for="">料理写真:</label>
                     <input type="file" class="form-control" name="food_img" >
                 </div>
                 <div class="form-grou mb-3">
+                    <label for="">店舗写真:</label>
                     <input type="file" class="form-control" name="shop_img" >
                 </div>
                 <div class="form-grou mb-3">
+                    <label for="">感想:</label>
                     <textarea name="thoughts"  maxlength="300" class="form-control" >{{$item->thoughts}}</textarea>
                 </div>
-                <div class="form-grou mb-3">
-                    お店の外観
-                    <img src={{ asset(optional($item->image)->shop_img) }} alt="">
-                </div>
+                <table class="table table-bordered">
+                    <tr>
+                        <td style="position:relative;width:25%;">
+                            <p style="position:absolute;top:50%;width:100%;text-align:center;">更新前の料理写真</p>
+                        </td>
+                        <td>
+                            <div style="text-align:center;">
+                                <img src={{ asset(optional($item->image)->food_img) }} alt="" style="width: 50%;height:50%;">
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="position:relative;width:25%;">
+                            <p style="position:absolute;top:50%;width:100%;text-align:center;">更新前のお店写真</p>
+                        </td>
+                        <td>
+                            <div style="text-align:center;">
+                                <img src={{ asset(optional($item->image)->shop_img) }} alt="" style="width: 50%;height:50%;">
+                            </div>
+                        </td>
+                    </tr>
+                </table>
                 <input type="submit"   class="btn btn-primary">
             </form>
         </div>
--- laravel-project/resources/views/content.blade.php
+++ laravel-project/resources/views/content.blade.php
@@ -3,6 +3,7 @@
 @section('title',"記録詳細")
 
 @section('content')
+<div class="border p-4 col-7 m-auto mt-5 mb-5">
     <!-- モーダルを開くボタン・リンク -->
     <a class="btn btn-danger" data-toggle="modal" data-target="#testModal">削除</a>
 
@@ -31,25 +32,25 @@
     <a class="btn btn-primary" href={{route('updateForm',['id' => $item->id])}} >更新</a>
     <div style="margin:auto">
         
-        <div class="" style="display:flex;justify-content:center;height:400px;">
-            <div class="col-8">
+        <div style="display:flex;justify-content:center;">
+            <div class="col-8 row-4">
                 <h2>{{$item->food_name}}</h2>
-                <img src={{ asset(optional($item->image)->food_img) }}  style="width: 100%;height:90%;" alt="">
+                <img src={{ asset(optional($item->image)->food_img) }}  style="width: 100%;" alt="">
             </div>
         </div>  
-            <div class="col-8" style="margin:auto">
-                <table class="table">
-                    <tr><th>店名</th><td>{{$item->shop_name}}</td></tr>
-                    <tr><th>料金</th><td>{{$item->price}}</td></tr>
-                    <tr><th>場所</th><td>{{$item->place}}</td></tr>
-                    <tr><th>訪問日</th><td>{{$item->visit_date}}</td></tr>
-                    <tr><th>感想</th><td>{{$item->thoughts}}</td></tr>
-                </table>
-            </div>
-            
-        </div>
-        <div class="" style="display:flex;justify-content:center">
-            <img src={{ asset(optional($item->image)->shop_img) }} class="col-8" alt="">
-        </div>
+        <div class="col-8" style="margin:auto">
+            <table class="table">
+                <tr><th>店名</th><td>{{$item->shop_name}}</td></tr>
+                <tr><th>料金</th><td>{{$item->price}}</td></tr>
+                <tr><th>場所</th><td>{{$item->place}}</td></tr>
+                <tr><th>訪問日</th><td>{{$item->visit_date}}</td></tr>
+                <tr><th>感想</th><td>{{$item->thoughts}}</td></tr>
+            </table>
+        </div>    
     </div>
+    <h2 class="col-8" style="margin:auto">{{$item->shop_name}}の店舗写真</h2>
+    <div class="col-8" style="margin:auto;display:flex;justify-content:center">
+        <img src={{ asset(optional($item->image)->shop_img) }} style="width: 100%;height:60%;" alt="">
+    </div>
+</div>
 @endsection
